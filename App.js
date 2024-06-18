1851fc2741286d489fbf576b880b0789d1b60ffc
anggario21d <anggario_21@apps.ipb.ac.id>
2024-06-18 14:43:07 +0700

    note app done

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import Home from "./screens/home";
+import AddNote from "./screens/addNote";
+import EditNote from "./screens/editNote";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const CurrentPageWidget = ({ currentPage, noteList, setCurrentPage, addNote, editNote, deleteNote, currentNote, setCurrentNote }) => {
+  switch (currentPage) {
+    case "home":
+      return <Home noteList={noteList} setCurrentPage={setCurrentPage} deleteNote={deleteNote} setCurrentNote={setCurrentNote} />;
+    case "add":
+      return <AddNote setCurrentPage={setCurrentPage} addNote={addNote} />;
+    case "edit":
+      return <EditNote item={currentNote} setCurrentPage={setCurrentPage} editNote={editNote} />;
+    default:
+      return <Home />;
+  }
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  const [currentPage, setCurrentPage] = useState("home");
+
+  const [noteList, setNoteList] = useState([
+    {
+      id: 1,
+      title: "Note pertama",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+    },
+  ]);
+
+  const [currentNote, setCurrentNote] = useState(null);
+
+  const addNote = (title, desc) => {
+    const id = noteList.length > 0 ? noteList[noteList.length - 1].id + 1 : 1;
+
+    setNoteList([
+      ...noteList,
+      {
+        id,
+        title: title,
+        desc: desc,
+      },
+    ]);
+  };
+
+  const editNote = (noteId, newTitle, newDesc) => {
+    const updatedNoteList = noteList.map((note) => (note.id == noteId ? { ...note, title: newTitle, desc: newDesc } : note));
+
+    setNoteList(updatedNoteList);
+  };
+
+  const deleteNote = (noteId) => {
+    const updatedNoteList = noteList.filter((note) => note.id !== noteId);
+
+    setNoteList(updatedNoteList);
+  };
+
+  return <CurrentPageWidget currentPage={currentPage} setCurrentPage={setCurrentPage} noteList={noteList} addNote={addNote} editNote={editNote} deleteNote={deleteNote} currentNote={currentNote} setCurrentNote={setCurrentNote} />;
+};
+
+export default App;
---
+++ screens/editNote.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import CustomTextInput from "../components/customTextInput";
+import CustomButton from "../components/customButton";
+
+const EditNote = ({ item, setCurrentPage, editNote }) => {
+  const [newTitle, setNewTitle] = useState(item.title);
+  const [newDesc, setNewDesc] = useState(item.desc);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.pageTitle}>Edit Note</Text>
+      <CustomTextInput text={newTitle} onChange={setNewTitle} label="Title" placeholder={item.title} numberOfLines={1} multiline={false} />
+      <CustomTextInput text={newDesc} onChange={setNewDesc} label="Description" placeholder={item.desc} multiline numberOfLines={4} />
+      <View style={styles.spacerTop}>
+        <CustomButton
+          backgroundColor="#247881"
+          color="#fff"
+          text="Save"
+          width="100%"
+          onPress={() => {
+            editNote(item.id, newTitle, newDesc);
+            setCurrentPage("home");
+          }}
+        />
+      </View>
+      <View style={styles.spacerTop}>
+        <CustomButton backgroundColor="#DDDDDD" color="#203239" text="Back to Home" width="100%" onPress={() => setCurrentPage("home")} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    padding: 20,
+  },
+  pageTitle: {
+    marginTop: 20,
+    fontSize: 20,
+    fontWeight: "700",
+    textAlign: "center",
+    color: "#203239",
+  },
+  spacerTop: {
+    marginTop: 30,
+  },
+});
+
+export default EditNote;
